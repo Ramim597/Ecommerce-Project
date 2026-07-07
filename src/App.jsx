a0977f6a872e66bc47c7d2eedb3c3a17665d0839
Ramim <191146698+Ramim597@users.noreply.github.com>
2026-07-07 20:08:19 +0600

    update the header section with a piace

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import "./Pages/Header.css";
 import { HomePage } from "./Pages/HomePage";
 import { CheckoutPage } from "./Pages/CheckoutPage";
+import { Order } from "./Pages/Order";
+import Tracking from "./Pages/Tracking";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route index element={<HomePage />} />
         <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="order" element={<Order />} />
+        <Route path="tracking" element={<Tracking />} />
       </Routes>
     </>
   );
